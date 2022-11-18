855d0fb247d021b04595921edee8af984cfed0fc
asunder123 <anand.sunder@capgemini.com>
2022-11-19 00:47:26 +0530

    Cleaned up code

--- src/main/python/Mongomicroservices/db.py
+++
@@ -1,9 +0,0 @@
-from conn  import Mongo
-
-class Database:
- 
- def db(self):
-  m=Mongo()
-  client=m.connection()
-  db = client["payment_1"]
-  return db
--- src/main/python/Mongomicroservices/table.py
+++
@@ -1,8 +0,0 @@
-from db import Database
-
-class Table:
- def table(self):
-  d=Database()
-  col=d.db()
-  col=col["customers"]
-  return col
